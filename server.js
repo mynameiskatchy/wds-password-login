ed37dc85f5e20e6dc0a6e8191dac3bcab871edc0
mynameiskatchy <karchietrinh@gmail.com>
2021-02-07 11:27:52 -0500

    Routes to add user credentials

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 const users = [{name : "Name"}]
 
+
+// get users
 app.get('/users', (req, res) => {
     res.json(users)
+})
+
+// create users
+app.post('/users', (req, res) => {
+    const user = {
+        name: req.body.name,
+        password: req.body.password
+    }
+    users.push(user)
+    res.status(201).send()
 })
 
 app.listen(3000)
