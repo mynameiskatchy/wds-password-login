83f8b301a01e08d29e1a53c40fbafc14f77aecaf
mynameiskatchy <karchietrinh@gmail.com>
2021-02-07 11:37:21 -0500

    Add brypt and salt and hash to user password

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { response } = require('express')
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
 app.use(express.json())
 
@@ -13,13 +14,18 @@ app.get('/users', (req, res) => {
 })
 
 // create users
-app.post('/users', (req, res) => {
-    const user = {
-        name: req.body.name,
-        password: req.body.password
+app.post('/users', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        console.log(salt)
+        console.log(hashedPassword)
+        const user = { name: req.body.name, password: hashedPassword }
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
     }
-    users.push(user)
-    res.status(201).send()
 })
 
 app.listen(3000)
